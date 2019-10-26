3a6b34ba9dfc47a67743b4a741553f27687f8e95
Andrew Capp <acapp909@gmail.com>
2019-10-26 20:09:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,7 @@
 # code here!
 require 'pry'
 class School
+<<<<<<< HEAD
   attr_accessor :roster
   attr_reader :sch_name
 
@@ -27,3 +28,44 @@ class School
     sorted_hash
   end
 end
+=======
+  #attr_accessor :roster
+  attr_reader :sch_name
+
+
+
+  def initialize(sch_name)
+    @sch_name = sch_name
+    #@roster = {}
+    roster = Hash.new
+
+  end
+
+  def roster
+    roster
+
+  end
+
+
+
+
+  def add_student(student_name, grade)
+
+    if roster.has_key?(grade)
+
+      roster[grade] << student_name
+
+    else
+
+      roster[grade] = []
+
+      roster[grade] << student_name
+    end
+
+  end
+
+end
+
+#my_sch = School.new("Miller")
+#p my_sch.add_student("Andy", 10)
+>>>>>>> d92cfa409053f2983c2039f0cd3b996cf5eed0bd
